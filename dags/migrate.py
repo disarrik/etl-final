fc2ef841bbcd967d98516d9e5a558459b2cedaf7
disarra <disarra02@mail.ru>
2025-03-15 09:06:54 +0300

    Добавил DAGS

---
+++ dags/migrate.py
@@ -0,0 +1,167 @@
+from airflow import DAG
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.operators.python import PythonOperator
+from pymongo import MongoClient
+from datetime import datetime
+import json
+from bson import Decimal128
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal128):
+            return float(obj.to_decimal())
+        elif isinstance(obj, datetime):
+            return obj.isoformat()
+        return super().default(obj)
+
+
+def transfer_mongo_to_postgres():
+    
+    mongo_client = MongoClient("mongodb://user:password@mongo:27017/")
+    mongo_db = mongo_client["db"]  
+
+    
+    postgres_hook = PostgresHook(postgres_conn_id="postgres_default")
+    conn = postgres_hook.get_conn()
+    cursor = conn.cursor()
+
+    
+    mongo_collection = mongo_db["UserSessions"]
+    mongo_data = list(mongo_collection.find({}))
+    for doc in mongo_data:
+        insert_query = """
+        INSERT INTO UserSessions (session_id, user_id, start_time, end_time, pages_visited, device, actions)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """
+        cursor.execute(insert_query, (
+            doc["session_id"],
+            doc["user_id"],
+            doc["start_time"],
+            doc.get("end_time"),
+            json.dumps(doc["pages_visited"]),
+            json.dumps(doc["device"]),
+            json.dumps(doc["actions"])
+        ))
+
+    
+    mongo_collection = mongo_db["ProductPriceHistory"]
+    mongo_data = list(mongo_collection.find({}))
+    for doc in mongo_data:
+        insert_query = """
+        INSERT INTO ProductPriceHistory (product_id, price_changes, current_price, currency)
+        VALUES (%s, %s, %s, %s)
+        """
+        cursor.execute(insert_query, (
+            doc["product_id"],
+            json.dumps(doc["price_changes"], cls=MyEncoder),
+            MyEncoder().default(doc["current_price"]),  
+            doc["currency"]
+        ))
+
+    
+    mongo_collection = mongo_db["EventLogs"]
+    mongo_data = list(mongo_collection.find({}))
+    for doc in mongo_data:
+        insert_query = """
+        INSERT INTO EventLogs (event_id, timestamp, event_type, details)
+        VALUES (%s, %s, %s, %s)
+        """
+        cursor.execute(insert_query, (
+            doc["event_id"],
+            doc["timestamp"],
+            doc["event_type"],
+            json.dumps(doc["details"])
+        ))
+
+    
+    mongo_collection = mongo_db["SupportTickets"]
+    mongo_data = list(mongo_collection.find({}))
+    for doc in mongo_data:
+        insert_query = """
+        INSERT INTO SupportTickets (ticket_id, user_id, status, issue_type, messages, created_at, updated_at)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)W
+        """
+        cursor.execute(insert_query, (
+            doc["ticket_id"],
+            doc["user_id"],
+            doc["status"],
+            doc["issue_type"],
+            json.dumps(doc["messages"], cls=MyEncoder),
+            doc["created_at"],
+            doc["updated_at"]
+        ))
+
+    
+    mongo_collection = mongo_db["UserRecommendations"]
+    mongo_data = list(mongo_collection.find({}))
+    for doc in mongo_data:
+        insert_query = """
+        INSERT INTO UserRecommendations (user_id, recommended_products, last_updated)
+        VALUES (%s, %s, %s)
+        """
+        cursor.execute(insert_query, (
+            doc["user_id"],
+            json.dumps(doc["recommended_products"]),
+            doc["last_updated"]
+        ))
+
+    
+    mongo_collection = mongo_db["ModerationQueue"]
+    mongo_data = list(mongo_collection.find({}))
+    for doc in mongo_data:
+        insert_query = """
+        INSERT INTO ModerationQueue (review_id, user_id, product_id, review_text, rating, moderation_status, flags, submitted_at)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        cursor.execute(insert_query, (
+            doc["review_id"],
+            doc["user_id"],
+            doc["product_id"],
+            doc["review_text"],
+            doc["rating"],
+            doc["moderation_status"],
+            json.dumps(doc["flags"]),
+            doc["submitted_at"]
+        ))
+
+    
+    mongo_collection = mongo_db["SearchQueries"]
+    mongo_data = list(mongo_collection.find({}))
+    for doc in mongo_data:
+        insert_query = """
+        INSERT INTO SearchQueries (query_id, user_id, query_text, timestamp, filters, results_count)
+        VALUES (%s, %s, %s, %s, %s, %s)
+        """
+        cursor.execute(insert_query, (
+            doc["query_id"],
+            doc["user_id"],
+            doc["query_text"],
+            doc["timestamp"],
+            json.dumps(doc["filters"]),
+            doc["results_count"]
+        ))
+
+    
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2023, 10, 1),
+    "retries": 1,
+}
+
+with DAG(
+    "mongo_to_postgres_transfer",
+    default_args=default_args,
+    schedule_interval="@daily",  
+    catchup=False,  
+) as dag:
+    transfer_task = PythonOperator(
+        task_id="transfer_mongo_to_postgres",
+        python_callable=transfer_mongo_to_postgres,
+    )
+
+    transfer_task
